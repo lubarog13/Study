80592fdfd14835351f13eae6088697703292024f
Lyubov Rogozina <lubarog13@gmail.com>
2022-10-02 20:51:52 +0300

    Работа с движением и RigidBody

--- games/Ball example/Assets/Scripts/Move.cs
+++ games/Ball example/Assets/Scripts/Move.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float speed;
+    public float forceValue;
+    private Rigidbody rb;
+    public float jumpValue;
+    private AudioSource audiosource;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f) {
+            rb.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+            audiosource.Play();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * forceValue);
     }
 }
