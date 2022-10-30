ab6743848e73d26d955cfe560754aa3cb58bce9f
Lyubov Rogozina <lubarog13@gmail.com>
2022-10-30 14:44:47 +0300

    Добавление стреляния врага по нам

--- games/Wallls/Assets/Scripts/WanderingAI.cs
+++ games/Wallls/Assets/Scripts/WanderingAI.cs
@@ -8,6 +8,9 @@ public class WanderingAI : MonoBehaviour
 
     public float obstacleRange = 5.0f;
 
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     private bool _alive;
     // Start is called before the first frame update
     void Start()
@@ -26,8 +29,15 @@ public class WanderingAI : MonoBehaviour
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                if (hit.distance < obstacleRange)
-                {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>()) {
+                    if(_fireball == null) {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange) {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
                 }
