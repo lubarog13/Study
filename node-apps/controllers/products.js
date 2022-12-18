30cd6629b1cab21bc5b2fb69d3b8876ed9511c6c
lubarog13 <lubarog13@gmail.com>
2022-12-18 18:31:01 +0300

    Добавление новых страниц для продуктов

--- node-apps/controllers/products.js
+++ node-apps/controllers/products.js
@@ -39,3 +39,23 @@ exports.getCheckout = (req, res, next) => {
         docTitle: 'Checkout'
     })
 }
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        docTitle: 'Your Orders'
+    });
+}
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            docTitle: 'Product',
+            path: '/products',
+            product: product,
+            activeShop: true,
+            productCSS: true,
+        })
+    })
+}
--- node-apps/routes/shop.js
+++ node-apps/routes/shop.js
@@ -5,6 +5,8 @@ const shopController = require('../controllers/products')
 
 router.get('/', shopController.getIndex)
 router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct)
 router.get('/card', shopController.getCard);
+router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 module.exports = router
