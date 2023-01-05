1bbfdf270ef9f303ff51d88c5b153b54f7e2f32f
lubarog13 <lubarog13@gmail.com>
2023-01-05 17:03:31 +0300

    Разработка скриптов для игры

---
+++ games/DestroyStones/Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    public static int currentNumberStonesThrown = 0;
+    public static int currentNumberDestroyedStones = 0;
+}
