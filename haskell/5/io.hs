6430bbda9463615f243bca3c5ad20ead25f225f0
lubarog13 <lubarog13@gmail.com>
2023-05-21 22:29:19 +0300

    IO конспект

--- haskell/5/io.hs
+++ haskell/5/io.hs
@@ -1,4 +1,6 @@
 module Demo where
+import Control.Monad
+
 
 main = do
     putStrLn "What is your name?"
@@ -14,3 +16,37 @@ main' = do
         main'
     else
         putStrLn $ "Hi, " ++ name ++ "!"
+
+
+{-
+    newtype IO a = RealWorld -> (RealWorld, a)
+
+    return :: a -> IO a
+    (>>=) :: a -> (a -> IO b) -> IO b
+
+    instance Monad IO where
+        return a = \w -> (w,a)
+        (>>=) m k = \w -> case m w of (w',a) -> k a w'
+
+-}
+
+getLine' :: IO String
+getLine' = do
+    c <- getChar
+    if c == '\n' then
+        return []
+    else do
+        cs <- getLine'
+        return (c:cs)
+
+putStr' :: String -> IO ()
+putStr' [] = return ()
+putStr' (x:xs) = putChar x >> putStr' xs
+
+
+putStr'' :: String -> IO ()
+putStr'' = sequence_ . map putChar
+
+
+putStr''' :: String -> IO ()
+putStr''' = mapM_ putChar
