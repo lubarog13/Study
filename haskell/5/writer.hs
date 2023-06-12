23393f5c5bbbab93c12632762b56ebe691254b7d
lubarog13 <lubarog13@gmail.com>
2023-06-12 19:34:18 +0300

    Writer monad

--- haskell/5/writer.hs
+++ haskell/5/writer.hs
@@ -3,7 +3,7 @@ module Demo where
 import Data.Monoid
 import Control.Monad
 
-newtype Writer w a = Writer { runWriter :: (a, w) }
+newtype Writer w a = Writer { runWriter :: (a, w) } deriving (Show, Eq)
 
 writer :: (a, w) -> Writer w a
 writer = Writer
@@ -28,3 +28,42 @@ instance (Monoid w) => Functor (Writer w) where
 
 evalWriter :: Writer w a -> a
 evalWriter m = fst (runWriter m)
+
+tell :: Monoid w => w -> Writer w ()
+tell w = writer ((), w)
+
+calc :: (Int -> Int -> Int) -> Int -> Int -> Writer String Int
+calc op arg1 arg2 = do
+  let res = arg1 `op` arg2
+  tell "ok"
+  if abs res < 128 then
+    return res
+  else do
+    tell "overflow"
+    return res
+    
+
+type Shopping = Writer (Sum Integer, [String]) ()
+
+shopping1 :: Shopping
+shopping1 = do
+  purchase "Jeans"   19200
+  purchase "Water"     180
+  purchase "Lettuce"   328
+
+purchase :: String -> Integer -> Shopping
+purchase item cost = writer((), (Sum cost, [item]))
+
+total :: Shopping -> Integer
+total = getSum . fst . execWriter
+
+items :: Shopping -> [String]
+items = snd . execWriter
+
+{-
+purchase :: String -> Integer -> Shopping
+purchase _ = tell . Sum
+
+total :: Shopping -> Integer
+total = getSum . execWriter
+-}
