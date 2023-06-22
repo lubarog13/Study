fd4f6e72fc931a3208c5ca89082dade25efbefb6
lubarog13 <lubarog13@gmail.com>
2023-06-22 22:11:54 +0300

    Новые стандарты конструкторов

--- c++_stepik/2/m2_1.cpp
+++ c++_stepik/2/m2_1.cpp
@@ -2,3 +2,18 @@
 
 typedef int * (Class::*Foo)(int, double) const;
 using Foo = int * (Class::*)(int, double) const;
+
+struct SomeType {
+    //SomeType() = default;
+    SomeType(int newNumber) : number(newNumber) {}
+    SomeType(): SomeType(42) {}
+    SomeType(OtherType value);
+    private:
+        int number;
+};
+
+struct NonCopyable {
+    NonCopyable() = default;
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable & operator=(const NonCopyable&) = delete;
+};
