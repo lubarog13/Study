fee75ac6b9eca6793c6b18c65dd7087320389431
lubarog13 <lubarog13@gmail.com>
2023-08-08 22:23:35 +0300

    Начало оформления главной страницы

---
+++ nuxt-blog/components/main/Post.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+    >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>{{ new Date().toLocaleString()}}</small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://docs.unity3d.com/cn/2021.1/uploads/Main/LightUsageSceneView.svg"
+        alt="post image"
+        class="post-img">
+    </div>
+    <footer class="post-footer">
+        <el-button round @click="openPost">Открыть</el-button>
+        <span>
+          <i class="el-icon-message">
+            12
+          </i>
+        </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .post {
+    min-height: 200px;
+    margin-bottom: 2rem;
+  }
+</style>
--- nuxt-blog/pages/index.vue
+++ nuxt-blog/pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="18" :md="12" :lg="10">
+        <app-post
+          v-for="post in 3"
+          :key="post"
+        />
+      </el-col>
+    </el-row>
 </template>
 
 <script>
+import AppPost from '@/components/main/Post.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head: {
+    title: 'Главная'
+  },
+  components: {AppPost}
 }
 </script>
