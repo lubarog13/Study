471f89f20b05a7eaa04a5819ddc711920139ed3b
lubarog13 <lubarog13@gmail.com>
2023-08-11 21:54:47 +0300

    Оформление страницы поста и комментариев

---
+++ nuxt-blog/components/main/Comment.vue
@@ -0,0 +1,36 @@
+<template>
+<el-card class="comment">
+  <div class="comment-header">
+    <span class="name">Person name</span>
+    <small><<i class="el-icon-time"></i> {{ new Date().toLocaleString()}}</small>
+  </div>
+  <div class="comment-text">
+    Lorem ipsum dolor sit amet.
+  </div>
+</el-card>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  props: ['comment']
+}
+</script>
+
+
+<style scoped lang="less">
+.comment {
+  margin-bottom: 1rem;
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: .5rem;
+    .name {
+      font-size: 0.8rem;
+    }
+  }
+  &-text {
+    white-space: pre-line;
+  }
+}
+</style>
--- nuxt-blog/pages/post/_id.vue
+++ nuxt-blog/pages/post/_id.vue
@@ -3,18 +3,42 @@
     <header class="post-header">
       <div class="post-title">
         <h1>Post title</h1>
-
+        <nuxt-link to="/"><i class="el-icon-back"></i></nuxt-link>
+      </div>
+      <div class="post-info">
+        <small><i class="el-icon-time"></i> {{ new Date().toLocaleString()}}</small>
+        <small><i class="el-icon-view"></i> 42</small>
+      </div>
+      <div class="post-image">
+        <img
+          src="https://docs.unity3d.com/cn/2021.1/uploads/Main/LightUsageSceneView.svg"
+          alt="post image">
       </div>
     </header>
-    <main>
-
+    <main class="post-text">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad consequatur consequuntur eius esse fugit, illum impedit incidunt iure libero minima natus nesciunt placeat porro possimus sed veniam? At, excepturi!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci doloremque est illum libero magnam molestiae non quod! Aut commodi consectetur deleniti dolorum est explicabo, iure nesciunt nisi quae vel velit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam, consectetur debitis ea expedita explicabo facere natus neque nesciunt, officia perferendis quis quo repellendus sed tempore vel vitae, voluptate voluptatem!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at cupiditate debitis delectus enim, et eum eveniet, hic ipsam, itaque necessitatibus nemo obcaecati pariatur perferendis praesentium quas tempora ut voluptatem?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eos ipsum laboriosam minus modi perferendis, placeat repellat sint unde. Blanditiis debitis delectus ipsum iste officia perspiciatis provident reiciendis saepe tempore!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus accusantium adipisci at consectetur cum dolores ducimus, est hic in libero nemo odit rem, repellat similique vel velit voluptas voluptatum!</p>
     </main>
-
+    <footer>
+      <!-- Form -->
+      <div class="comments" v-if="true">
+        <app-comment v-for="comment in 4" :key="comment" :comment="comment"/>
+      </div>
+      <div class="text-center" v-else>Комментариев нет</div>
+    </footer>
   </article>
 </template>
 
 <script>
+import AppComment from '@/components/main/Comment.vue'
 export default {
+  components: {
+    AppComment
+  },
   validate({params}) {
     return Boolean(params.id)
   }
@@ -23,6 +47,29 @@ export default {
 
 <style scoped lang="less">
 .post {
-
+  max-width: 600px;
+  margin: 0 auto;
+  &-title, &-info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    align-items: center;
+  }
+  &-info {
+    margin-bottom: .5rem;
+  }
+  &-image img {
+    width: 100%;
+    height: auto;
+  }
+  &-header {
+    margin-bottom: 1.5rem;
+  }
+  &-content {
+    margin-bottom: 2rem;
+  }
+  &-text {
+    margin-bottom: 1.5rem;
+  }
 }
 </style>
