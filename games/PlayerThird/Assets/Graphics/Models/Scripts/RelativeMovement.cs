6a4316d8612bd654083aed3cd87c310654435f89
lubarog13 <lubarog13@gmail.com>
2023-09-09 21:41:30 +0300

    Добавлен экран, меняющий цвет

--- games/PlayerThird/Assets/Graphics/Models/Scripts/RelativeMovement.cs
+++ games/PlayerThird/Assets/Graphics/Models/Scripts/RelativeMovement.cs
@@ -12,6 +12,7 @@ public class RelativeMovement : MonoBehaviour
     public float gravity = -9.8f;
     public float terminalVelocity = -10.0f;
     public float minFall = -1.02f;
+    public float pushForce = 3.0f;
 
     private float _vertSpeed;
     private ControllerColliderHit _contact;
@@ -91,5 +92,9 @@ public class RelativeMovement : MonoBehaviour
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         _contact = hit;
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body!=null && !body.isKinematic) {
+            body.velocity = hit.moveDirection * pushForce;
+        }
     }
 }
