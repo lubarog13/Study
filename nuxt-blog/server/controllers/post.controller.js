51c582bb08f16f393fcf02d18fc4bd70884489c9
lubarog13 <lubarog13@gmail.com>
2023-10-14 22:15:04 +0300

    Добавление картинок

--- nuxt-blog/server/controllers/post.controller.js
+++ nuxt-blog/server/controllers/post.controller.js
@@ -1,13 +1,35 @@
 const Post = require('../models/post.model');
 
 module.exports.create = async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: `/${req.file.filename}`,
+    })
 
+    try {
+        await post.save();
+        res.status(201).json(post);
+    } catch(e) {
+        res.status(400).json(e);
+    }
 };
 
 module.exports.getAll = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1})
+        res.json(posts)
+    } catch(e) {
+        res.status(200).json(e)
+    }
 };
 
 module.exports.getById = async (req, res) => {
+    try {
+        await Post.findById(req.params.id).populate('comments')
+    } catch (e) {
+        res.status(500).json(e)
+    }
 };
 
 module.exports.update = async (req, res) => {
