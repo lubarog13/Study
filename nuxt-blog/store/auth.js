942b74f0478c06c412ebdaa8cb40b9bb6cd71b57
lubarog13 <lubarog13@gmail.com>
2023-11-16 22:21:42 +0300

    Сохранение токена

--- nuxt-blog/store/auth.js
+++ nuxt-blog/store/auth.js
@@ -1,3 +1,7 @@
+import Cookie from  'cookie';
+import Cookies from 'js-cookie'
+import {jwtDecode} from 'jwt-decode'
+
 export const state = () => ({
   token: null, //чтобы каждый раз не входить
 });
@@ -11,16 +15,22 @@ export const actions = {
   async login({commit, dispatch}, formData) {
     try {
       const {token} = await this.$axios.$post('/api/auth/admin/login', formData)
-      this.$axios.setToken(token, 'Bearer')
-      commit('updateToken', token);
+      console.log(token)
+      dispatch('setToken', token)
     } catch (e) {
       commit('setError', e, {root: true})
       throw e
     }
   },
+  setToken({commit}, token) {
+    this.$axios.setToken(token, 'Bearer')
+    commit('updateToken', token);
+    Cookies.set('jwt-token', token);
+  },
   logout({commit}) {
     this.$axios.setToken(false)
     commit('clearToken')
+    Cookies.remove('jwt-token');
   },
   async createUser({commit}, formData) {
     try {
@@ -29,6 +39,18 @@ export const actions = {
       commit('setError', e, {root: true})
       throw e;
     }
+  },
+  autoLogin({dispatch}) {
+    const cookieStr = process.browser
+      ? document.cookie
+      : this.app.context.req.headers.cookie;
+    const cookies = Cookie.parse(cookieStr || '') || {}
+    const token = cookies['jwt-token']
+    if(isJWTValid(token)) {
+     dispatch('setToken', token);
+    } else {
+      dispatch('logout')
+    }
   }
 }
 
@@ -36,4 +58,14 @@ export const getters = {
   isAuthenticated: (state) =>  {
     return Boolean(state.token);
   }
+}
+
+function isJWTValid(token) {
+  if (!token) {
+    return false
+  }
+
+  const jwtData = jwtDecode(token) || {}
+  const expires = jwtData.exp || 0
+  return (new Date().getTime() / 1000) < expires;
 }
