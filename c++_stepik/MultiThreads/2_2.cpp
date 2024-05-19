d83c785ddd188ee803617baeefce2920fc05dcbf
lubarog13 <lubarog13@gmail.com>
2024-05-19 17:23:41 +0300

    Умные указатели в C++

--- c++_stepik/MultiThreads/2_2.cpp
+++ c++_stepik/MultiThreads/2_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 template <class Type>
 class SmartPointer {
@@ -68,4 +69,27 @@ int main(int argc, char** argv) {
     std::cout << *sp1 << std::endl;
     std::cout << sp2->length() << std::endl;
     std::cout << *sp2 << std::endl;
+    std::unique_ptr<Foo> sp11(new Foo());
+    std::unique_ptr<Foo> sp21;
+    //sp2 = sp1; error
+    sp21 = std::move(sp11);
+    sp21.reset();
+    std::shared_ptr<Foo> PFoo(new Foo());
+    std::shared_ptr<Foo> PFoo2;
+    PFoo2 = PFoo;
+    PFoo2.reset();
+    PFoo.reset();
+    std::shared_ptr<int> p1(new int(5));
+    std::weak_ptr<int> wp1 = p1;
+    {
+        std::shared_ptr<int> p2 = wp1.lock();
+        if(p2) {
+            //..
+        }
+    }
+    p1.reset();
+    std::shared_ptr<int> p3 = wp1.lock();
+    if(p3) {
+        //...
+    }
 }
